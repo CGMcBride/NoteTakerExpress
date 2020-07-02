cf6fa5d9807eab7d60f5e6e256a2919d7b09491b
CGMcBride <connermcbride@ymail.com>
2020-07-01 19:01:13 -0500

    created POST for API

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const fs = require("fs")
 const app = express()
 const path = require("path")
 const PORT = process.env.PORT || 3000;
@@ -17,6 +18,18 @@ app.get('/', function (req, res) {
 app.get('/notes', function (req, res) {
     res.sendFile(path.join(__dirname, "./public/notes.html"))
 })
+// I am creating the API Routes
+app.get("/api/notes", function (req, res) {
+    res.json(database)
+})
+// I will create a post for our API
+app.post("/api/notes", function (req, res) {
+    database.push(req.body)
+    fs.writeFile("./db/db.json", JSON.stringify(database), function (err) {
+        if (err) throw err
+    })
+})
+
 app.listen(PORT, () => {
     console.log('Port is listening at 3000');
 });
